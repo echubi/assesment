b372df4d63a0a643c604b7ef043b3e569bc4802a
echubi <echubiojo@gmail.com>
2024-05-21 00:28:00 +0100

    Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
 	};
   return (
 		<section className={`${styles.paddingY}`}>
-			<div className="px-[15px] pt-[230px] pb-[50px] ">
+			<div className="px-[15px] ss:pt-[230px] pb-[50px] ">
 				<div className="flex flex-col w-full ">
-					<h2 className="font-poppins font-bold xs:text-[40px] ss:text-[60px] text-black xs:leading-[70px] ss:leading-[100px] leading-[15px] xs:pb-[20px] text-center">
+					<h2 className="font-poppins font-bold xs:text-[40px] ss:text-[60px] text-black xs:leading-[70px] ss:leading-[100px]  xs:pb-[20px] text-center">
 						Welcome To My Fincra Assessment
 					</h2>
-					<p className="font-poppins text-[20px] text-black text-center xs:py-5">
+					<p className="font-poppins text-[20px] text-black text-center">
 						This is to assess my frontend skills for a Junior Frontend Developer role
 					</p>
 					<div className="m-[auto] block">
